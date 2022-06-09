c59ae281ac062e06bd9e9857439855bdd266c07a
CHKao <climnehcc234@gmail.com>
2022-06-10 02:21:24 +0800

    avoid zero prob:ok numerical value:ok missing value:not done

--- p3/NB.py
+++ p3/NB.py
@@ -3,6 +3,7 @@
 import sys
 import pandas as pd
 import numpy as np
+from statistics import NormalDist
 
 #read raw training data
 df = pd.read_csv(sys.argv[1], header=None)
@@ -18,19 +19,60 @@ for i in range(len(type)):
         unique_list.append(None)
 
 total_dict = {}
+c_probility = {}
 for i in class_list:
     total_dict[i] = []
     temp_df = df[df[len(type)] == str(i)]
+    c_probility[i] = len(temp_df) / len(df)
     for j in range(len(type)):
+        #categorical features
         if type[j] == 0:
             assert(unique_list[j] is not None)
             _dict = {}
+            if_zero_value = False
             for k in unique_list[j]:
                 _dict[k] = len(temp_df[temp_df[j] == k])
+                if _dict[k] == 0:
+                    if_zero_value = True
+            #avoid zero possibility
+            if if_zero_value:
+                for k in unique_list[j]:
+                    _dict[k] += 1
+            #calculate conditional possibility
+            sum = 0
+            for k in unique_list[j]:
+                sum += _dict[k]
+            for k in unique_list[j]:
+                _dict[k] /= sum
             total_dict[i].append(_dict)
+        #numerical features
         else:
-            total_dict[i].append(None)
-for key, value in total_dict.items():
-    print(key)
-    print(value)
-    print()
+            total_dict[i].append({
+                'mean': temp_df[j].mean(), 
+                'std': temp_df[j].std()
+            })
+
+#testing
+df_test = pd.read_csv(sys.argv[2], header=None)
+df_test = df_test.drop([len(type)], axis=1)
+result = ['class']
+
+
+for i in range(1, len(df_test)):
+    x = df_test.iloc[i].to_numpy()
+    predict = {}
+    for j in class_list:
+        possibility = c_probility[j]
+        for val, index in zip(x, range(len(x))):
+            #categorical features
+            if type[index] == 0:
+                possibility *= total_dict[j][index][val]
+            else:
+                possibility *= NormalDist(mu=total_dict[j][index]['mean'], \
+                    sigma=total_dict[j][index]['std']).pdf(val)
+        predict[j] = possibility
+
+    result.append(max(predict, key=predict.get))
+
+df_test[13] = result
+df_test.to_csv(sys.argv[3], header=False, index=False)
